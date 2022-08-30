72b038c1b54970e7e9fcbbb14c251278a6ee6951
bv-pulkit <pulkit@blogvault.net>
2022-08-30 13:33:50 +0000

    Changes

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,5 +1,28 @@
 ActiveAdmin.register Article do
+	index do
+		selectable_column
+		index_column
+		column :title
+		column :body
+		column "User" do |article|
+			link_to "#{article.user_id} : #{article.user.email}", admin_user_path(article.user_id)
+		end
+	end
 
+	filter :title
+	filter :user_id
+
+	show do
+			attributes_table do |article|
+				row :title
+				row :body
+				#row "User Detail" do |article|
+				#	link_to "#{article.user_id} : #{article.user.email}", admin_user_path(article.user_id)
+				#end
+		end
+	end
+
+	#	filter :email, :as => :select
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,17 +1,35 @@
 ActiveAdmin.register User do
+	index do
+		column :email, sortable: :title
+		actions do |user|
+			item "users", admin_user_path(user)
+		end
+		column :user
+		column :articles_count do |user|
+			user.articles.count
+		end
+	end
 
+	show do
+		attributes_table do
+			row :email
+			row :created_at
+			row :Article_Count do |user|
+				link_to "#{ user.articles.count }",admin_articles_path("q[id_equals]" => article.user_id)
+			end
+		end
+	end
 	# See permitted parameters documentation:
 	# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 	#
 	# Uncomment all parameters which should be permitted for assignment
-	#
+
 	# permit_params :email, :password_digest, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
-	#
 	# or
 	#
-	permit_params do
-		permitted = [:email, :password_digest, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
-		permitted << :other if params[:action] == 'create' && current_user.admin?
-		permitted
-	end
+	#permit_params do
+	#permitted = [:email, :password_digest, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
+	#permitted << :other if params[:action] == 'create' && current_user.admin?
+	#permitted
+	#end
 end
